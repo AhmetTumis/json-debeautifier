e245ba411e9e241ed7f6d24e2796f53fa31c933e
Ahmet Tümiş <56443860+AhmetTumis@users.noreply.github.com>
2024-10-13 11:52:59 +0300

    extension document editing logic added

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "json-debeautifier" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('json-debeautifier.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from JSON Debeautifier!');
+	const disposable = vscode.commands.registerCommand('json-debeautifier.debeautifyMyJSON', () => {
+
+		vscode.window.showInformationMessage('JSON Debeautifier do be working eh?');
+	
+		const editor = vscode.window.activeTextEditor;
+
+		if(!editor) {
+			vscode.window.showErrorMessage('No active editor found.');
+			return;
+		}
+	
+		const document = editor.document;
+		const selection = editor.selection;
+		const selectedText = document.getText(selection) || document .getText();
+
+		try {
+
+			const jsonObject = JSON.parse(selectedText);
+			const collapsedJSON = JSON.stringify(jsonObject);
+
+			editor.edit(editBuilder => {
+				if(!selection.isEmpty){
+					editBuilder.replace(selection, collapsedJSON);
+				} else {
+					const firstLine = document.lineAt(0);
+					const lastLine = document.lineAt(document.lineCount - 1);
+					const fullRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+					editBuilder.replace(fullRange, collapsedJSON);
+				}
+			});
+		} catch(error){
+			vscode.window.showErrorMessage('Invalid JSON selected. Please select a valid JSON');
+		}
 	});
 
 	context.subscriptions.push(disposable);
+
 }
 
 // This method is called when your extension is deactivated
